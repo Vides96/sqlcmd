ea22a7e9a8b44fe5c6aad71e8f74cf0d34f0e796
curinaupa <curinaupa@gmail.com>
2018-04-05 17:53:17 +0300

    now, we revice the number of parametrs

--- src/ua/com/sqlcmd/controller/MainController.java
+++ src/ua/com/sqlcmd/controller/MainController.java
@@ -19,23 +19,30 @@ public class MainController {
         view.write("Hello user");
         view.write("Input, names of your database, username and password in next format: database|userName|password");
         while (true) {
-            String string = view.read();
-            String[] data = string.split("\\|");
-            String databaseName = data[0];
-            String userName = data[1];
-            String password = data[2];
             try {
+                String string = view.read();
+                String[] data = string.split("\\|");
+                if (data.length != 3) {
+                    throw new IllegalArgumentException("number of parameters  don't match divided with, '|', must be 3 but we have: " + data.length);
+                }
+                String databaseName = data[0];
+                String userName = data[1];
+                String password = data[2];
                 manager.connect(databaseName, userName, password);
                 break;
             } catch (Exception e) {
-                String message = e.getMessage();
-                if (e.getCause() != null) {
-                    message += " " + e.getCause().getMessage();
-                }
-                view.write("Fault, maybe " + message);
-                view.write("Enter again your datas");
+                printError(e);
             }
         }
         view.write("Ok");
+    }
+
+    private void printError(Exception e) {
+        String message = e.getMessage();
+        if (e.getCause() != null) {
+            message += " " + e.getCause().getMessage();
+        }
+        view.write("Fault, maybe " + message);
+        view.write("Enter again your datas");
     }
 }
