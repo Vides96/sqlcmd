d29d3f891ded62052696a71d98aa1f9d0b4e6c0a
curinaupa <curinaupa@gmail.com>
2018-05-05 11:11:19 +0300

    coverage 100% for command clear

--- test/ua/com/sqlcmd/controller/command/FakeView.java
+++ test/ua/com/sqlcmd/controller/command/FakeView.java
@@ -4,6 +4,8 @@ import ua.com.sqlcmd.view.View;
 
 public class FakeView implements View {
     private String messages = "";
+    private String input = null;
+
     @Override
     public void write(String message) {
         messages += message + "\n";
@@ -11,7 +13,16 @@ public class FakeView implements View {
 
     @Override
     public String read() {
-        return null;
+        if (input == null) {
+            throw new IllegalStateException("reset method read");
+        }
+        String result = input;
+        this.input = null;
+        return result;
+    }
+
+    public void addRead(String input) {
+        this.input = input;
     }
 
     public String getContent() {
